1882af08c06461b9df7a878b8debc606fa2ccbd4
Gray Schafer <jacobwschafer@sbcglobal.net>
2017-04-12 08:15:03 -0400

    Create NormalPerson.java

---
+++ NormalPerson.java
@@ -0,0 +1,15 @@
+package W17Project4StudentHelp;
+
+public class NormalPerson extends Person{
+	public NormalPerson(Eatery num1, int num2, int num3, int num4){
+		super(num1,num2,num3,num4);
+	}
+
+	@Override
+	public void event(int tick) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
