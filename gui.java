1d04e139d1168f3147f6884ae93362040554aaaf
Dez <whitede@mail.gvsu.edu>
2017-04-11 22:20:17 -0400

    pt 1 of 2, need to add bottom buttons/fields

--- gui.java
+++ gui.java
@@ -19,8 +19,8 @@ public class GUI {
             pane.setComponentOrientation(ComponentOrientation.RIGHT_TO_LEFT);
         }
  
-        JButton button;
-        JTextField inputInfo,  sec2Next, avgSecPerReg, totalInSecs, avgSecPerEat, secB4depart, NumberOfPlaces;
+        JButton beginSim, endSim;
+        JTextField inputInfo, NumberOfPlacesOutput, secB4departOutput, avgSecPerEatOutput, totalInSecsOutput, avgSecPerRegOutput, secsToNextPerson, sec2Next, avgSecPerReg, totalInSecs, avgSecPerEat, secB4depart, NumberOfPlaces, inputInfoOutput;
     pane.setLayout(new GridBagLayout());
     pane.setBackground(Color.BLACK);
     GridBagConstraints c = new GridBagConstraints();
@@ -29,7 +29,7 @@ public class GUI {
     c.fill = GridBagConstraints.HORIZONTAL;
     }
  
-    inputInfo = new JTextField(" Input Information:");
+    inputInfo = new JTextField(" Input Information:  ");
     inputInfo.setEditable(false);
     if (shouldWeightX) {
     c.weightx = 0.5;
@@ -38,16 +38,43 @@ public class GUI {
     c.gridx = 0;
     c.gridy = 0;
     pane.add(inputInfo , c);
- 
-    sec2Next = new JTextField(" Seconds to next person:");
+    
+    inputInfoOutput = new JTextField("                   	----------------------------");
+    inputInfoOutput.setEditable(false);
+    inputInfoOutput.setBackground(Color.BLACK);
+    inputInfoOutput.setForeground(Color.white);
+    if (shouldWeightX) {
+    c.weightx = 0.5;
+    }
+    c.fill = GridBagConstraints.HORIZONTAL;
+    c.gridx = 1;
+    c.gridy = 0;
+    pane.add(inputInfoOutput , c);
+    
+  
+    pane.add(inputInfoOutput , c);
+    sec2Next = new JTextField("  Seconds to next person:  ");
     sec2Next.setEditable(false);
     c.fill = GridBagConstraints.HORIZONTAL;
     c.weightx = 0.5;
     c.gridx = 0;
     c.gridy = 1;
     pane.add(sec2Next, c);
+    
+    secsToNextPerson = new JTextField("                   Temp to be replaced with backend accessor");
+    secsToNextPerson.setEditable(false);
+    secsToNextPerson.setBackground(Color.BLACK);
+    secsToNextPerson.setForeground(Color.white);
+    if (shouldWeightX) {
+    c.weightx = 0.5;
+    }
+    c.fill = GridBagConstraints.HORIZONTAL;
+    c.gridx = 1;
+    c.gridy = 1;
+    pane.add(secsToNextPerson , c);
  
-    avgSecPerReg = new JTextField(" Average Seconds per cashier:");
+ 
+    avgSecPerReg = new JTextField(" Average Seconds per cashier:  ");
     avgSecPerReg.setEditable(false);
     c.fill = GridBagConstraints.HORIZONTAL;
     c.weightx = 0.5;
@@ -55,7 +82,19 @@ public class GUI {
     c.gridy = 2;
     pane.add(avgSecPerReg, c);
     
-    totalInSecs = new JTextField("Total time in seconds");
+    avgSecPerRegOutput = new JTextField("                   Temp to be replaced with backend accessor");
+    avgSecPerRegOutput.setEditable(false);
+    avgSecPerRegOutput.setBackground(Color.BLACK);
+    avgSecPerRegOutput.setForeground(Color.white);
+    if (shouldWeightX) {
+    c.weightx = 0.5;
+    }
+    c.fill = GridBagConstraints.HORIZONTAL;
+    c.gridx = 1;
+    c.gridy = 2;
+    pane.add(avgSecPerRegOutput , c);
+    
+    totalInSecs = new JTextField("Total time in seconds  ");
     totalInSecs.setEditable(false);
     c.fill = GridBagConstraints.HORIZONTAL;
     c.weightx = 0.5;
@@ -63,7 +102,19 @@ public class GUI {
     c.gridy = 3;
     pane.add(totalInSecs, c);
     
-    avgSecPerEat = new JTextField("Average Seconds per eatery:");
+    totalInSecsOutput = new JTextField("                   Temp to be replaced with backend accessor");
+    totalInSecsOutput .setEditable(false);
+    totalInSecsOutput .setBackground(Color.BLACK);
+    totalInSecsOutput .setForeground(Color.white);
+    if (shouldWeightX) {
+    c.weightx = 0.5;
+    }
+    c.fill = GridBagConstraints.HORIZONTAL;
+    c.gridx = 1;
+    c.gridy = 3;
+    pane.add(totalInSecsOutput  , c);
+    
+    avgSecPerEat = new JTextField("Average Seconds per eatery:  ");
     avgSecPerEat.setEditable(false);
     c.fill = GridBagConstraints.HORIZONTAL;
     c.weightx = 0.5;
@@ -71,7 +122,19 @@ public class GUI {
     c.gridy = 4;
     pane.add(avgSecPerEat, c);
     
-    secB4depart = new JTextField("Seconds before person leaves:");
+    avgSecPerEatOutput = new JTextField("                   Temp to be replaced with backend accessor");
+    avgSecPerEatOutput .setEditable(false);
+    avgSecPerEatOutput .setBackground(Color.BLACK);
+    avgSecPerEatOutput.setForeground(Color.white);
+    if (shouldWeightX) {
+    c.weightx = 0.5;
+    }
+    c.fill = GridBagConstraints.HORIZONTAL;
+    c.gridx = 1;
+    c.gridy = 4;
+    pane.add(avgSecPerEatOutput  , c);
+    
+    secB4depart = new JTextField("Seconds before person leaves:  ");
     secB4depart.setEditable(false);
     c.fill = GridBagConstraints.HORIZONTAL;
     c.weightx = 0.5;
@@ -79,7 +142,19 @@ public class GUI {
     c.gridy = 5;
     pane.add(secB4depart, c);
     
-    NumberOfPlaces = new JTextField("Number of restaurants: ");
+    secB4departOutput = new JTextField("                   Temp to be replaced with backend accessor");
+    secB4departOutput .setEditable(false);
+    secB4departOutput .setBackground(Color.BLACK);
+    secB4departOutput.setForeground(Color.white);
+    if (shouldWeightX) {
+    c.weightx = 0.5;
+    }
+    c.fill = GridBagConstraints.HORIZONTAL;
+    c.gridx = 1;
+    c.gridy = 5;
+    pane.add(secB4departOutput  , c);
+    
+    NumberOfPlaces = new JTextField("Number of restaurants:   ");
     NumberOfPlaces.setEditable(false);
     c.fill = GridBagConstraints.HORIZONTAL;
     c.weightx = 0.5;
@@ -87,18 +162,41 @@ public class GUI {
     c.gridy = 6;
     pane.add(NumberOfPlaces, c);
     
- 
-    button = new JButton("5");
-    c.fill = GridBagConstraints.HORIZONTAL;
-    c.ipady = 0;       //reset to default
-    c.weighty = 1.0;   //request any extra vertical space
-    c.anchor = GridBagConstraints.PAGE_END; //bottom of space
-    c.insets = new Insets(10,0,0,0);  //top padding
-    c.gridx = 1;       //aligned with button 2
-    c.gridwidth = 2;   //2 columns wide
-    c.gridy = 2;       //third row
-    pane.add(button, c);
+    NumberOfPlacesOutput = new JTextField("                   Temp to be replaced with backend accessor");
+    NumberOfPlacesOutput .setEditable(false);
+    NumberOfPlacesOutput .setBackground(Color.BLACK);
+    NumberOfPlacesOutput.setForeground(Color.white);
+    if (shouldWeightX) {
+    c.weightx = 0.5;
     }
+    c.fill = GridBagConstraints.HORIZONTAL;
+    c.gridx = 1;
+    c.gridy = 6;
+    pane.add(NumberOfPlacesOutput  , c);
+    
+    
+   beginSim = new JButton("Begin Simulation");
+   Dimension panelD = new Dimension(80,180);
+   beginSim.setPreferredSize(panelD);
+   // beginSim.setEditable(false);
+   c.fill = GridBagConstraints.HORIZONTAL;
+    c.weightx = 1;
+    c.gridx = 1;
+    c.gridy = 8;
+    pane.add(beginSim, c);
+    
+    
+    endSim = new JButton("End Simulation");
+    // beginSim.setEditable(false);
+    c.fill = GridBagConstraints.HORIZONTAL;
+     c.weightx = 0;
+     c.gridx = 2;
+     c.gridy = 8;
+     pane.add(endSim, c);
+    
+    
+    }
+   
  
     /**
      * Create the GUI and show it.  For thread safety,
@@ -107,7 +205,7 @@ public class GUI {
      */
     private static void createAndShowGUI() {
         //Create and set up the window.
-        JFrame frame = new JFrame("");
+        JFrame frame = new JFrame("GridBagLayoutDemo");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
  
         //Set up the content pane.
