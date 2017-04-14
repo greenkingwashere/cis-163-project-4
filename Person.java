ffae1174528fa72365f90ff784eed2ac78225caf
Dez <whitede@mail.gvsu.edu>
2017-04-14 02:55:34 -0400

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,26 +1,31 @@
- /**
+/**
  * 
  */
 package W17Project4StudentHelp;
 
-public abstract class Person implements ClockListener{
+public abstract class Person implements ClockListener {
 	int tickTime;
 	Eatery Destination;
 	protected int cashiersTime;
 	protected int leaveTime;
 	protected int eateryTime;
-	
-	//public abstract void setCashiersTime(int time);
-	//public abstract void setLeaveTime(int time);
-	//public abstract void setEateryTime(int time);
-	
-	public Person(Eatery num1, int num2, int num3, int num4){
-		/**@param num1 the Eatery the person is going to
-		 * @param num2 the time for cashier
-		 * @param num3 the leave time
-		 * @param num4 the eatery time
+
+	// public abstract void setCashiersTime(int time);
+	// public abstract void setLeaveTime(int time);
+	// public abstract void setEateryTime(int time);
+
+	public Person(Eatery num1, int num2, int num3, int num4) {
+		/**
+		 * @param num1
+		 *            the Eatery the person is going to
+		 * @param num2
+		 *            the time for cashier
+		 * @param num3
+		 *            the leave time
+		 * @param num4
+		 *            the eatery time
 		 *
-		 *      
+		 * 
 		 **/
 		tickTime = 0;
 		Destination = num1;
@@ -28,30 +33,17 @@ public abstract class Person implements ClockListener{
 		leaveTime = num3;
 		eateryTime = num4;
 	}
-	public void KILL(){//kill the person
+
+	public void KILL() {// kill the person
 		Sim.line.remove(this);
-		
-		
-		
+
 	}
-	
-	/**
-	public double getLeaveTime() {
-		return leaveTime;
-	}
-	public Eatery getDestination() {
-		return Destination;
-	}
-	public void setDestination(Eatery destination) {
-		Destination = destination;
-	}
-	public int getTickTime() {
-		return tickTime;
-	}
-	public void setTickTime(int tickTime) {
-		this.tickTime = tickTime;
-	}
-	public void setEateryTime(double time) {
-		this.leaveTime = (int) time;
-	}
-	*/
+}
+
+/**
+ * public double getLeaveTime() { return leaveTime; } public Eatery
+ * getDestination() { return Destination; } public void setDestination(Eatery
+ * destination) { Destination = destination; } public int getTickTime() { return
+ * tickTime; } public void setTickTime(int tickTime) { this.tickTime = tickTime;
+ * } public void setEateryTime(double time) { this.leaveTime = (int) time; }
+ */
