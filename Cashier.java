3b2c346e8643656b05cc739eb9df8d90fe5bd998
Dez <whitede@mail.gvsu.edu>
2017-04-14 02:47:45 -0400

    Update Cashier.java

--- Cashier.java
+++ Cashier.java
@@ -1,33 +1,37 @@
 package W17Project4StudentHelp;
-public class Cashier implements ClockListener{
-	
+
+public class Cashier implements ClockListener {
+
 	static int numberCompleted = 0;
 	private int timeOfNextEvent = 0;
 	private Person customer;
 	boolean ghost = false;
-	
-	public void add(Person pal){
-		if (customer == null){
+
+	public void add(Person pal) {
+		if (customer == null) {
 			customer = pal;
 			ghost = true;
-			System.out.print("Added person: "+customer);
+			System.out.print("Added person: " + customer);
 		}
 	}
-	public boolean isFull(){
-		if (customer != null) return true;
-		else return false;
+
+	public boolean isFull() {
+		if (customer != null)
+			return true;
+		else
+			return false;
 	}
-	
+
 	@Override
 	public void event(int tick) {
-		if (ghost){
+		if (ghost) {
 			timeOfNextEvent = tick + customer.cashiersTime;
 			ghost = false;
 		}
-		if (timeOfNextEvent >= tick){
-			//numberCompleted++;
+		if (timeOfNextEvent >= tick) {
+			// numberCompleted++;
 			customer = null;
-			
+
 		}
 	}
 
