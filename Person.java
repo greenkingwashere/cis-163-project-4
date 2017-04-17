7ee57aef88a47691adbb3a3f308ea6e089363dca
Dez <whitede@mail.gvsu.edu>
2017-04-17 05:31:15 -0400

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,6 +1,4 @@
-/**
- * 
- */
+
 package W17Project4StudentHelp;
 
 public abstract class Person implements ClockListener {
@@ -10,23 +8,8 @@ public abstract class Person implements ClockListener {
 	protected int leaveTime;
 	protected int eateryTime;
 
-	// public abstract void setCashiersTime(int time);
-	// public abstract void setLeaveTime(int time);
-	// public abstract void setEateryTime(int time);
-
 	public Person(Eatery num1, int num2, int num3, int num4) {
-		/**
-		 * @param num1
-		 *            the Eatery the person is going to
-		 * @param num2
-		 *            the time for cashier
-		 * @param num3
-		 *            the leave time
-		 * @param num4
-		 *            the eatery time
-		 *
-		 * 
-		 **/
+	
 		tickTime = 0;
 		Destination = num1;
 		cashiersTime = num2;
@@ -34,16 +17,8 @@ public abstract class Person implements ClockListener {
 		eateryTime = num4;
 	}
 
-	public void KILL() {// kill the person
+	public void KILL() {
 		Sim.line.remove(this);
 
 	}
 }
-
-/**
- * public double getLeaveTime() { return leaveTime; } public Eatery
- * getDestination() { return Destination; } public void setDestination(Eatery
- * destination) { Destination = destination; } public int getTickTime() { return
- * tickTime; } public void setTickTime(int tickTime) { this.tickTime = tickTime;
- * } public void setEateryTime(double time) { this.leaveTime = (int) time; }
- */
